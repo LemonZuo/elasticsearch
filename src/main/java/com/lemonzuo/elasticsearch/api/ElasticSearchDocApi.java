70341e3101f21ff4aa8b9bb3ca8f829b6f3bdf24
LemonZuo <lemonzuo@qq.com>
2021-04-04 10:35:32 +0800

    批操作、精确查询

--- src/main/java/com/lemonzuo/elasticsearch/api/ElasticSearchDocApi.java
+++ src/main/java/com/lemonzuo/elasticsearch/api/ElasticSearchDocApi.java
@@ -1,28 +1,43 @@
 package com.lemonzuo.elasticsearch.api;
 
+import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson.JSON;
 import com.lemonzuo.elasticsearch.model.User;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.elasticsearch.core.query.HighlightQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author LemonZuo
@@ -110,5 +125,48 @@ public class ElasticSearchDocApi {
         DeleteRequest deleteRequest = new DeleteRequest("db_api", "1");
         DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
         log.info("{}", deleteResponse.status());
+    }
+
+    /**
+     * 批量插入文档
+     * @throws IOException
+     */
+    @GetMapping("batchAddDoc")
+    public void batchAddDoc() throws IOException {
+        BulkRequest request = new BulkRequest();
+        request.timeout("10s");
+        List<User> userList = new ArrayList<>(3);
+        userList.add(new User("11", 111));
+        userList.add(new User("22", 222));
+        userList.add(new User("33", 333));
+        userList.forEach(user -> {
+            IndexRequest indexRequest = new IndexRequest("db_api")
+                    .id(IdUtil.objectId())
+                    .source(JSON.toJSONString(user), XContentType.JSON);
+            request.add(indexRequest);
+        });
+
+        BulkResponse responses = client.bulk(request, RequestOptions.DEFAULT);
+        log.info("{}", responses.status());
+    }
+
+    @GetMapping("searchDoc")
+    public String searchDoc() throws IOException {
+        SearchRequest searchRequest = new SearchRequest("db_api");
+        // 构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        // 精确查询
+        TermQueryBuilder query = QueryBuilders.termQuery("name", "11");
+
+        searchSourceBuilder.query(query);
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        // 分页
+         searchSourceBuilder.from();
+         searchSourceBuilder.size();
+
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        return JSON.toJSONString(response);
+
     }
 }
